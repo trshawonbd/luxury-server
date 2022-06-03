f5712ec297f9b64183734c3cdddea53c4fb3add5
Your Name <you@example.com>
2022-06-04 00:20:16 +0300

    user and jwt created

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const jwt = require('jsonwebtoken');
 
 
 const app = express();
@@ -20,10 +21,35 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try {
       await client.connect();
-      console.log("DB CONNECTED")
+      console.log("DB CONNECTED");
+
+      const userCollection = client.db('luxury').collection('user');
+
+
+      app.put('/user/:email', async(req, res) =>{
+          const email = req.params.email;
+          const user = req.body;
+          const filter = {email: email};
+          const options = {upsert: true};
+
+          const updateDoc = {
+              $set : user,
+          }
+
+          console.log(user);
+
+          const result = await userCollection.updateOne(filter, updateDoc, options);
+
+          const token = jwt.sign({email: email}, process.env.ACCESS_TOKEN, {expiresIn: '1hr'});
+
+          res.send({ result, token }); 
+
+      })
+
       
     } finally {
-      await client.close();
+      /* await client.close();
+     */
     }
   }
   run().catch(console.dir);
