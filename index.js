afe9fce443268669a09a3142c3c538a960a5eec4
Your Name <you@example.com>
2022-06-05 13:41:55 +0300

    get single service

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const jwt = require('jsonwebtoken');
 
 
@@ -77,6 +77,15 @@ async function run() {
       app.get('/service', verifyJWT, async(req,res) =>{
         const query = {};
         const result = await serviceCollection.find(query).toArray();
+        res.send(result);
+      })
+
+      app.get(`/service/:id`, async(req, res) =>{
+        const id = req.params.id;
+        const query = {_id: ObjectId(id)};
+
+        const result = await serviceCollection.findOne(query);
+
         res.send(result);
       })
 
